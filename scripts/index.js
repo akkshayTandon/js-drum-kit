07074e2a762bdabcc30cf52edbab357c30861f70
Akkshay Tandon <76265879+akkshayTandon@users.noreply.github.com>
2023-05-16 11:49:37 +0530

    updated index.js to play sound on mobile devices

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove("effect");
@@ -14,5 +16,17 @@ function playSound(e) {
 
 const keys = Array.from(document.getElementsByClassName("key"));
 keys.forEach(k => k.addEventListener("transitionend", removeTransition));
+keys.forEach(key => key.addEventListener("touch", playMouseClick));
+keys.forEach(key => key.addEventListener("click", playMouseClick));
+
 window.addEventListener("keypress", playSound); 
-// window.addEventListener("touchstart", playSound); 
+
+function playMouseClick(event) {
+    const button = event.target.attributes[0].nodeValue;
+    const audio = document.querySelector(`audio[data-key="${button}"]`)
+    const key = document.querySelector(`.key[data-key="${button}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; 
+    key.classList.add("effect");
+    audio.play();
+}
